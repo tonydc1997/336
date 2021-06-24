2146bd20022108f8ab96814b092635a1b949d973
CJ <34591992+cj2big@users.noreply.github.com>
2021-06-24 18:28:17 -0500

    Update system.js

--- system.js
+++ system.js
@@ -1,47 +1,46 @@
+let records = ["phone-numbers"];
+
 //let x = location.search;
 //let x = location.hash;
-let x = location.search.replace("?", "/");
-if (x != "") {
-}
-function database(section){
-  let x = document.getElementById("main-body");
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // Typical action to be performed when the document is ready:
-      console.log(xhttp);
-      x.innerHTML = xhttp.responseText;
-    }else if(this.readyState == 4 && this.status == 404){
-      x.innerHTML = xhttp.responseText;
+let hash = location.hash.substr(1);
+console.log(hash);
+hashCheck(hash);
+window.addEventListener(
+  "hashchange",
+  function () {
+    console.log("The hash has changed!");
+    hashCheck();
+  },
+  false
+);
+function hashCheck() {
+  hash = location.hash.substr(1);
+  console.log("checking hash " + hash);
+  if (hash != "") {
+    if (hash.includes("&") == false) {
+      console.log(records.indexOf(hash))
+      if (records.indexOf(hash) !== -1) {
+        if (hash == "phone-numbers") {
+          body.innerHTML = htmlNumbers;
+        }
+      }
     }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
-/*function body(target, url) {
-  let x = document.getElementById("main-body");
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // Typical action to be performed when the document is ready:
-      console.log(xhttp);
-      x.innerHTML = xhttp.responseText;
-    }else if(this.readyState == 4 && this.status == 404){
-      x.innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}*/
-/*window.addEventListener('hashchange', function() {
-  let x = location.hash.replace("#", "/");
-  console.log(`Hash changed to: ${x}`);
-  if(x != ""){
-    xhttpReq("#main-body", x)
   }
-}, false);
-*/
-console.log("Success");
+}
+
+function load(url) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      x.innerHTML = xhttp.responseText;
+    } else if (this.readyState == 4 && this.status == 404) {
+      x.innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
 console.log(`
 ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
